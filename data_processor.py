92334d133f1fc83a776cfc09dee03f01caf193fc
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-07 13:04:36 -0400

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -3,8 +3,17 @@ class DataProcessor:
   def __init__(self):
     pass
 
-  def get_interval_avg(self, interval):
+  def get_interval_avg(self, data, interval):
     """Returns the avg blood-glucose level in a given time interval"""
 
 if __name__ == '__main__':
-  dp = DataProcessor()
+  # IMPORT MODULE
+  from data_handler import DataHandler
+
+  filename = 'test_db.csv'
+  dh = DataHandler(filename)
+
+  # GET DATA FROM PROFILE 1
+  data = dh.get_data_by_profile(1)
+
+  dp = DataProcessor(data)
