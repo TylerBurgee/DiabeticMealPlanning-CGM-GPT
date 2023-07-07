00c945a12e55c4f3f1c929aeb106e3851a1a078f
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-07 13:00:26 -0400

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -1,1 +1,10 @@
+class DataProcessor:
 
+  def __init__(self):
+    pass
+
+  def get_interval_avg(self, interval):
+    """Returns the avg blood-glucose level in a given time interval"""
+
+if __name__ == '__main__':
+  dp = DataProcessor()
