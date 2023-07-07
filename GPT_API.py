9841e3acbf2ba7bde3dc92679e8268564535de69
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-07 12:51:39 -0400

    Update GPT_API.py

--- GPT_API.py
+++ GPT_API.py
@@ -1,1 +1,13 @@
+import openai
 
+openai.organization = 'ORGANIZATION_ID_HERE'
+openai.api_key = 'API_KEY_HERE'
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}
+  ]
+)
+
+print(completion.choices[0].message.content)
