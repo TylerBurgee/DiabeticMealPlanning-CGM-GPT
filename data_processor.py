6b0e2d0c9927bd6aa411b0ddce17300d542663a6
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-11 14:50:26 -0400

    Update data_processor.py
    Fixed a bug that occurred when the lower-bound of the interval was greater than zero. Updated documentation.

--- data_processor.py
+++ data_processor.py
@@ -8,12 +8,16 @@ class DataProcessor:
 
   @staticmethod
   def get_interval_avg(data: list, interval: tuple) -> float:
-    """Returns the average blood-glucose level in a given time interval"""
+    """
+    Returns the average blood-glucose level in a given time interval.
+    Lower bound on the interval is inclusive, upper bound is exclusive. 
+    Interval values should be multiples of the time_increment variable.
+    """
     time_increment = 5 # TIME BETWEEN EACH CGM READING
     sum_of_readings = 0
     num_of_readings = (interval[1] - interval[0]) / time_increment
 
-    for x in range(int(num_of_readings)):
+    for x in range(int(interval[0] / time_increment), int(interval[1] / time_increment)):
       sum_of_readings += float(data[x])
 
     avg = sum_of_readings / num_of_readings
@@ -30,5 +34,5 @@ if __name__ == '__main__':
   # GET DATA FROM A SINGLE DAY
   data = dh.get_data_by_day(0, 1)
 
-  interval_avg = DataProcessor.get_interval_avg(data, (0, 60))
+  interval_avg = DataProcessor.get_interval_avg(data, (10, 20))
   print(interval_avg)
