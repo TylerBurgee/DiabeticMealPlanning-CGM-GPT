79dee6f81e9793a38163734444e66b78a35209d0
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-12 09:24:25 -0400

    Update data_processor.py
    Made changes to draw_patient_graph_day() function. It is now working properly.

--- data_processor.py
+++ data_processor.py
@@ -39,66 +39,26 @@ class DataProcessor:
 
     return avg
   
-  def draw_patient_graph_day(graph_patient: int, graph_profile: int) -> None:
+  def draw_patient_graph_day(patient_data) -> None:
         """
         Draws a graph of the given patient's CGM readings over the day.
         Note: the first patient is 0.
         """
-        patient_data = dh.get_data_by_patient(patient=graph_patient, profile=graph_profile)
+        patient_data = [float(datum) for datum in patient_data]
 
-        # start_time = datetime.strptime('12:00', '%H:%M')
-        # time_ticks = [start_time + timedelta(minutes=5 * i) for i in range(len(patient_data))]
-
-        start_time = datetime.strptime('12:00 PM', '%I:%M %p')
-        time_list = []
+        start_time = datetime.strptime('12:00 AM', '%I:%M %p')
+        times_list = []
         current_time = start_time
 
-        prev = patient_data[0]
         for i in range(len(patient_data)):
-            time_list.append(current_time.strftime('%I:%M %p'))
+            times_list.append(current_time.strftime('%I:%M %p'))
             current_time += timedelta(minutes=5)
-            # if prev > patient_data[i+1]:
-            #   print("LOOK HERE")
-            # prev = patient_data[i+1]
 
-        N = len(patient_data)
-
-        plt.figure(figsize=(12, 6))
-
-        # plt.plot(patient_data)
-
-        sns.scatterplot(x=time_list, y=patient_data)
+        plt.bar(times_list, patient_data)
+        plt.title('Patient Data Over Time')
         plt.xlabel('Time')
         plt.ylabel('Patient Data')
-        plt.title('Patient Data over Time (Seaborn)')
         plt.show()
-
-        # plt.xticks(time_list)
-        # plt.yticks(patient_data)
-        # git commit -m "updated draw_patient_graph_day, x is to be the time and y is to be the glucose level"
-        # plt.grid()
-
-        # plt.gca().margins(x=0)
-        # plt.gcf().canvas.draw()
-        # tl = plt.gca().get_xticklabels()
-        # maxsize = max([t.get_window_extent().width for t in tl])
-        # m = 0.2  # Inch margin
-        # s = maxsize / plt.gcf().dpi * N + 2 * m
-        # margin = m / plt.gcf().get_size_inches()[0]
-
-        # plt.gcf().subplots_adjust(left=margin, right=1. - margin)
-        # plt.gcf().set_size_inches(s, plt.gcf().get_size_inches()[1])
-
-        # plt.xlabel('Time')
-        # plt.ylabel('CGM Reading')
-        # plt.title('CGM Readings over the Day')
-
-        # plt.yticks(fontsize=8)
-
-        # plt.tight_layout()
-        # plt.show()
-
-
 
 if __name__ == '__main__':
   # IMPORT MODULES
@@ -107,14 +67,14 @@ if __name__ == '__main__':
   filename = 'test_db.csv'
   dh = DataHandler(filename)
 
-  # GET DATA FROM A SINGLE DAY
-  data = dh.get_data_by_patient(patient=0, profile=1)
+  # GET DATA FROM A SINGLE PATIENT (ONE DAY)
+  patient_data = dh.get_data_by_patient(patient=0, profile=1)
 
-  interval_avg = DataProcessor.get_interval_avg(data, (10, 20))
+  interval_avg = DataProcessor.get_interval_avg(patient_data, (10, 20))
   print("Average from minutes 10-20:", interval_avg)
 
-  day_avg = DataProcessor.get_day_avg(data)
+  day_avg = DataProcessor.get_day_avg(patient_data)
   print("Day average:", day_avg)
 
-  day_graph = DataProcessor.draw_patient_graph_day(graph_patient=0, graph_profile=1)
+  day_graph = DataProcessor.draw_patient_graph_day(patient_data)
   print("Graph drawn", day_graph)
