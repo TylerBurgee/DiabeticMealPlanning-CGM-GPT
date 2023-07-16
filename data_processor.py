dd3dcfb8a9344833b7f3a765d3ea23c7910c3006
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-15 21:19:51 -0400

    Update data_processor.py
    Made a draw_patient_graph_interval() function and remade the draw_patient_graph_day() function to use the interval function. Worked on formatting the graphs. In the interval function, you can specify how many x-axis labels to show, using the tick_factor variable. The day function shows each hour on the x-axis.

--- data_processor.py
+++ data_processor.py
@@ -2,6 +2,7 @@
 Author: Tyler J. Burgee, Vaageesha Das
 Date: 11 July 2023
 """
+
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
@@ -37,28 +38,34 @@ class DataProcessor:
 
     return avg
 
-  def draw_patient_graph_interval(patient_data, patient_profile, patient_id, start_time, end_time) -> None:
-        """Draws a graph of the given patient's CGM readings over the day"""
-        patient_data = [float(datum) for datum in patient_data]
+  def draw_patient_graph_interval(data: list, interval: tuple, time_increment=5, tick_factor=6) -> None:
+    """Draws a graph of the given patient's CGM readings over a given time interval"""
+    data = [float(datum) for datum in data]
 
-        starting_data_index = "12:00 AM" - start_time
-        times_list = []
-        current_time = start_time
+    start_time = datetime.strptime('12:00pm', '%I:%M%p')
+    times_list = []
+    current_time = start_time
 
-        for i in range(len(patient_data)):
-            times_list.append(current_time.strftime('%I:%M %p'))
-            current_time += timedelta(minutes=5)
+    for i in range(len(data)):
+        times_list.append(current_time.strftime('%I:%M\n%p'))
+        current_time += timedelta(minutes=time_increment)
 
-        plt.bar(times_list, patient_data)
-        plt.title('Patient Data Over Time')
-        plt.xlabel('Time')
-        plt.ylabel('Patient Data')
-        plt.show()
+    data = data[int(interval[0] / time_increment):int(interval[1] / time_increment)]
+    times_list = times_list[int(interval[0] / time_increment):int(interval[1] / time_increment)]
+    print(data)
 
-  def draw_patient_graph_day(patient_data, patient_profile, patient_id):
-     start_time = datetime.strptime('12:00 AM', '%I:%M %p')
-     end_time = datetime.strptime('11:55 AM', '%I:%M %p')
-     DataProcessor.draw_patient_graph_interval(patient_data, patient_profile, patient_id, start_time, end_time)
+    plt.bar(times_list, data)
+    plt.title('Patient Data Over Time')
+    plt.xlabel('Time')
+    plt.ylabel('Patient Data')
+    plt.xticks([time for x,time in enumerate(times_list) if x%tick_factor==0])
+    plt.margins(x=0)
+    plt.show()
+
+  def draw_patient_graph_day(data: list) -> None:
+    """Draws a graph of the given patient's CGM readings over the day"""
+    interval = (0, 1440)
+    DataProcessor.draw_patient_graph_interval(data, interval, tick_factor=12)
 
 if __name__ == '__main__':
   # IMPORT MODULES
@@ -68,7 +75,7 @@ if __name__ == '__main__':
   dh = DataHandler(filename)
 
   # GET DATA FROM A SINGLE PATIENT (ONE DAY)
-  patient_data = dh.get_data_by_patient(patient=0, profile=1)
+  patient_data = dh.get_data_by_patient(patient=0, profile=2)
 
   interval_avg = DataProcessor.get_interval_avg(patient_data, (10, 20))
   print("Average blood-glucose from minutes 10-20:", interval_avg)
@@ -76,5 +83,8 @@ if __name__ == '__main__':
   day_avg = DataProcessor.get_day_avg(patient_data)
   print("Average blood-glucose for the day:", day_avg)
 
-  # GRAPH PATIENT DATA
+  # GRAPH PATIENT DATA OVER 1-HOUR TIME INTERVAL
+  DataProcessor.draw_patient_graph_interval(patient_data, (0, 60))
+
+  # GRAPH PATIENT DATA OVER AN ENTIRE DAY
   DataProcessor.draw_patient_graph_day(patient_data)
