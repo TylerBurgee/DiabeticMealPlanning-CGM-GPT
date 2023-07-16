f2f87bec2f33b75bba85977cf31f7a8f99f8973a
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-16 17:27:20 -0400

    Update data_processor.py
    Created helper functions _get_start_count_() and _get_times_list_() to reduce code redundancy. Restructured get_interval_avg() and get_day_avg() to use datetime objects for the time interval, instead of a tuple.

--- data_processor.py
+++ data_processor.py
@@ -1,6 +1,5 @@
 """
-Author: Tyler J. Burgee, Vaageesha Das
-Date: 11 July 2023
+Authors: Tyler J. Burgee, Vaageesha Das
 """
 
 import matplotlib.pyplot as plt
@@ -10,32 +9,52 @@ class DataProcessor:
   """Class to process synthetic CGM data"""
 
   @staticmethod
-  def get_interval_avg(data: list, interval: tuple, time_increment=5) -> float:
-    """
-    Returns the average blood-glucose level in a given time interval.
-    Lower bound on the interval is inclusive, upper bound is exclusive. 
-    Interval values should be multiples of the time_increment variable.
+  def _get_start_count_(start_time: object) -> int:
+    """Returns the number of increments between current_time and start_time"""
+    current_time = datetime.strptime('12:00 PM', '%I:%M %p')
+    counter = 0
 
-    data: a single day of CGM data in list format.
-    interval: time interval over which the average is to be calculated.
-    time_increment: time in between each CGM reading.
-    """
+    while current_time < start_time:
+      current_time += timedelta(minutes=5)
+      counter += 1
+
+    return counter
+
+  @staticmethod
+  def _get_times_list_(start_time: object, end_time: object) -> list:
+    """Returns a list of discrete times in between the given time interval"""
+    current_time = start_time
+    times_list = []
+
+    while current_time <= end_time:
+      times_list.append(current_time.strftime('%I:%M %p'))
+      current_time += timedelta(minutes=5)
+
+    return times_list
+
+  @staticmethod
+  def get_interval_avg(patient_data: list, start_time: object, end_time: object) -> float:
+    """Returns the average blood-glucose level over a given time interval"""
+    patient_data = [float(datum) for datum in patient_data]
+    counter = DataProcessor._get_start_count_(start_time)
+    times_list = DataProcessor._get_times_list_(start_time, end_time)
+    num_of_readings = len(times_list)
     sum_of_readings = 0
-    num_of_readings = (interval[1] - interval[0]) / time_increment
 
-    for x in range(int(interval[0] / time_increment), int(interval[1] / time_increment)):
-      sum_of_readings += float(data[x])
+    for i in range(len(times_list)):
+      sum_of_readings += patient_data[counter+i]
 
     avg = sum_of_readings / num_of_readings
 
     return avg
 
   @staticmethod
-  def get_day_avg(data: list, time_increment=5) -> float:
+  def get_day_avg(patient_data: list) -> float:
     """Returns the average blood-glucose level in a given day"""
-    interval = (0, 1440)
+    start_time = datetime.strptime('12:00 PM', '%I:%M %p')
+    end_time = datetime.strptime('11:55 PM', '%I:%M %p')
 
-    avg = DataProcessor.get_interval_avg(data, interval, time_increment)
+    avg = DataProcessor.get_interval_avg(patient_data, start_time, end_time)
 
     return avg
 
@@ -43,22 +62,8 @@ class DataProcessor:
   def draw_patient_graph_interval(patient_data: list, start_time: object, end_time: object) -> None:
     """Draws a graph of the given patient's CGM readings over a specified time interval"""
     patient_data = [float(datum) for datum in patient_data]
-    current_time = datetime.strptime('12:00 AM', '%I:%M %p')
-    times_list = []
-    counter = 0
-
-    # STORES THE NUMBER OF INCREMENTS BETWEEN current_time AND start_time
-    while current_time <= start_time:
-      current_time += timedelta(minutes=5)
-      counter += 1
-
-    current_time = start_time
-
-    # CREATES A LIST OF DISCRETE TIMES IN BETWEEN THE TIME INTERVAL
-    while current_time <= end_time:
-      times_list.append(current_time.strftime('%I:%M %p'))
-      current_time += timedelta(minutes=5)
-
+    counter = DataProcessor._get_start_count_(start_time)
+    times_list = DataProcessor._get_times_list_(start_time, end_time)
     patient_interval_data = []
 
     for i in range(len(times_list)):
@@ -82,8 +87,8 @@ class DataProcessor:
   @staticmethod
   def draw_patient_graph_day(patient_data: list) -> None:
     """Draws a graph of the given patient's CGM readings over an entire day"""
-    start_time = datetime.strptime('12:00 AM', '%I:%M %p')
-    end_time = datetime.strptime('11:55 AM', '%I:%M %p')
+    start_time = datetime.strptime('12:00 PM', '%I:%M %p')
+    end_time = datetime.strptime('11:55 PM', '%I:%M %p')
     DataProcessor.draw_patient_graph_interval(patient_data, start_time, end_time)
 
 if __name__ == '__main__':
@@ -96,9 +101,15 @@ if __name__ == '__main__':
   # GET DATA FROM A SINGLE PATIENT (ONE DAY)
   patient_data = dh.get_data_by_patient(patient=0, profile=2)
 
-  interval_avg = DataProcessor.get_interval_avg(patient_data, (10, 20))
-  print("Average blood-glucose from minutes 10-20:", interval_avg)
+  # CREATE START AND END TIME OBJECTS
+  start_time = datetime.strptime('3:00 PM', '%I:%M %p')
+  end_time = datetime.strptime('5:00 PM', '%I:%M %p')
 
+  # GET THE AVERAGE BLOOD-GLUCOSE LEVEL FROM 3:00 PM to 5:00 PM
+  interval_avg = DataProcessor.get_interval_avg(patient_data, start_time, end_time)
+  print("Average blood-glucose from 3:00 PM to 5:00 PM:", interval_avg)
+
+  # GET THE AVERAGE BLOOD-GLUCOSE LEVEL OVER AN ENTIRE DAY
   day_avg = DataProcessor.get_day_avg(patient_data)
   print("Average blood-glucose for the day:", day_avg)
 
@@ -106,7 +117,4 @@ if __name__ == '__main__':
   DataProcessor.draw_patient_graph_day(patient_data)
 
   # GRAPH PATIENT DATA OVER THE TIME INTERVAL FROM 3:00 PM to 5:00 PM
-  start_time = datetime.strptime('3:00 PM', '%I:%M %p')
-  end_time = datetime.strptime('5:00 PM', '%I:%M %p')
   DataProcessor.draw_patient_graph_interval(patient_data, start_time, end_time)
-
