807364562897d498b7adbe436b306b83f3a4eabf
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-18 16:31:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,24 @@ from data_handler import DataHandler
 from data_processor import DataProcessor
 from patient import Patient
 from datetime import datetime, timedelta, time
+from gpt_api import GPT_API
 
 class Main:
 
-    def _create_prompt_(patient: object) -> str:
+    def __init__(self, api_key, org_id):
+        self.api_key = api_key
+        self.org_id = org_id
+        self.gpt = GPT_API(api_key, org_id)
+
+    def blood_glucose_increment_test(self, increment=5):
+        """
+        Slowly increments blood-glucose level to determine ChatGPT's thresholds
+        for what it considers healthly/unhealthy/severe blood-glucose.
+        """
+        pass
+
+    def instantaneous_blood_glucose_prompt(self, patient: object):
+        """Sends an instantaneous CGM reading to ChatGPT and requests meal suggestions."""
         blood_glucose = 'Blood-glucose: {} mg/dL.\n'.format(patient.get_current_blood_glucose())
         last_meal = 'Last meal: {} hr ago.\n'.format(patient.get_hours_since_last_meal())
         query = 'Query: Is this safe? If so, recommend meals for breakfast (If blood-glucose is severely low or high, prioritize quickly raising or lowering blood-sugar levels,respectively, before eating a meal); if not, what should I do(prioritize seeking medical attention over remedies)?\n'
@@ -27,23 +41,11 @@ class Main:
         if patient.get_medication() is not None:
             prompt += 'Medication: {}.\n'.format(patient.get_medication())
 
-        return prompt
+        response = self.gpt.send_prompt(prompt)
 
-    def blood_glucose_increment_test(increment=5):
-        """
-        Slowly increments blood-glucose level to determine ChatGPT's thresholds
-        for what it considers healthly/unhealthy/severe blood-glucose.
-        """
-        pass
+        return response
 
-    def instantaneous_blood_glucose_test(patient: object):
-        """Sends an instantaneous CGM reading to ChatGPT and requests meal suggestions."""
-        patient_data = patient.get_data()
-        last_meal_time = patient.get_last_meal_time()
-
-        prompt = Main._create_prompt_(patient)
-
-    def average_blood_glucose_test(patient: object):
+    def average_blood_glucose_test(self, patient: object):
         """Sends an average of CGM readings since last meal to ChatGPT and requests meal suggestions."""
         pass
 
@@ -51,14 +53,13 @@ if __name__ == '__main__':
     filename = 'test_db.csv'
     dh = DataHandler(filename)
 
+    api_key = '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'
+    org_id = 'org-cMRrZweyGUcQPXCwXcub7hc9'
+
+    main = Main(api_key, org_id)
+
     last_meal_time = datetime.strptime('3:00 PM', '%I:%M %p')
     diabetic_patient = Patient(dh.get_data_by_patient(patient=0, profile=4), last_meal_time=last_meal_time, dietary_restrictions='vegan', budget='20')
 
-    prompt = Main._create_prompt_(diabetic_patient)
-    print(prompt)
-
-    api_key = ''
-    org_id = ''
-    gpt = GPT_API(api_key, org_id)
-    response = gpt.send_prompt(prompt)
+    response = main.instantaneous_blood_glucose_prompt(diabetic_patient)
     print(response)
