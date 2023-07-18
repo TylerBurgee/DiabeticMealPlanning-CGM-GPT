1a3deab824068fce4756f1b33a3f937e16a3b0a2
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-18 16:23:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ Date: 11 July 2023
 from data_handler import DataHandler
 from data_processor import DataProcessor
 from patient import Patient
+from datetime import datetime, timedelta, time
 
 class Main:
 
     def _create_prompt_(patient: object) -> str:
-        blood_glucose = 'Blood-glucose: {} mg/dL.\n'.format(patient.get_instantaneous_blood_glucose(0))
-        last_meal = 'Last meal: {} hr ago.\n'.format(patient.get_last_meal())
+        blood_glucose = 'Blood-glucose: {} mg/dL.\n'.format(patient.get_current_blood_glucose())
+        last_meal = 'Last meal: {} hr ago.\n'.format(patient.get_hours_since_last_meal())
         query = 'Query: Is this safe? If so, recommend meals for breakfast (If blood-glucose is severely low or high, prioritize quickly raising or lowering blood-sugar levels,respectively, before eating a meal); if not, what should I do(prioritize seeking medical attention over remedies)?\n'
 
         prompt = blood_glucose + last_meal + query
@@ -37,7 +38,10 @@ class Main:
 
     def instantaneous_blood_glucose_test(patient: object):
         """Sends an instantaneous CGM reading to ChatGPT and requests meal suggestions."""
-        pass
+        patient_data = patient.get_data()
+        last_meal_time = patient.get_last_meal_time()
+
+        prompt = Main._create_prompt_(patient)
 
     def average_blood_glucose_test(patient: object):
         """Sends an average of CGM readings since last meal to ChatGPT and requests meal suggestions."""
@@ -47,11 +51,14 @@ if __name__ == '__main__':
     filename = 'test_db.csv'
     dh = DataHandler(filename)
 
-    healthy_patient = Patient(dh.get_data_by_patient(patient=0, profile=1), last_meal=10, dietary_restrictions='vegan', budget='20')
+    last_meal_time = datetime.strptime('3:00 PM', '%I:%M %p')
+    diabetic_patient = Patient(dh.get_data_by_patient(patient=0, profile=4), last_meal_time=last_meal_time, dietary_restrictions='vegan', budget='20')
 
-    prompt = Main._create_prompt_(healthy_patient)
+    prompt = Main._create_prompt_(diabetic_patient)
     print(prompt)
 
     api_key = ''
     org_id = ''
-    #gpt = GPT_API(api_key, org_id)
+    gpt = GPT_API(api_key, org_id)
+    response = gpt.send_prompt(prompt)
+    print(response)
