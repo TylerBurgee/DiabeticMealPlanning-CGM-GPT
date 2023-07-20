070e50ee05c0ffed1ae4da22096060ccc4949897
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-19 21:58:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 """
 Author: Tyler J. Burgee
-Date: 11 July 2023
 """
 
 # IMPORT MODULES
@@ -24,7 +23,7 @@ class Main:
         """
         log = []
 
-        for x in range(21):
+        for x in range(400):
             if x % 10 == 0:
                 prompt = "My fasting blood-glucose level is {} mg/dL.".format(x)
                 response = self.gpt.send_prompt(prompt)
@@ -32,30 +31,28 @@ class Main:
 
         return log
 
-    def instantaneous_blood_glucose_prompt(self, patient: object, meal_inquiry: str) -> str:
+    def get_meal_suggestions(self, patient: object, meal_inquiry: str) -> str:
         """Sends an instantaneous CGM reading to ChatGPT and requests meal suggestions."""
-        blood_glucose = 'Blood-glucose: {} mg/dL.\n'.format(patient.get_current_blood_glucose())
+        current_blood_glucose = 'Current blood-glucose: {} mg/dL.\n'.format(patient.get_current_blood_glucose())
+        start_blood_glucose = 'Blood-glucose at {}: {}.\n'.format(patient.get_last_meal_time_str(), patient.get_blood_glucose_at_time(patient.get_last_meal_time()))
+        avg_blood_glucose = 'Average blood-glucose since last meal: {}.\n'.format(patient.get_avg_blood_glucose_last_meal())
         last_meal = 'Last meal: {} hr ago.\n'.format(patient.get_hours_since_last_meal())
         query = 'Query: Is this safe? If so, recommend meals for {} (If blood-glucose is severely low or high, prioritize quickly raising or lowering blood-sugar levels,respectively, before eating a meal); if not, what should I do(prioritize seeking medical attention over remedies)?\n'.format(meal_inquiry)
 
-        prompt = blood_glucose + last_meal + query
+        prompt = current_blood_glucose + start_blood_glucose + avg_blood_glucose + last_meal + query
 
-        if patient.get_dietary_restrictions() is not None:
+        if patient.get_dietary_restrictions() != "":
             prompt += 'Dietary restrictions: {}.\n'.format(patient.get_dietary_restrictions())
-        if patient.get_budget() is not None:
+        if patient.get_budget() != "":
             prompt += 'Budget: ${}.\n'.format(str(patient.get_budget()))
-        if patient.get_exercise() is not None:
+        if patient.get_exercise() != "":
             prompt += 'Exercise: {}.\n'.format(patient.get_exercise())
-        if patient.get_medication() is not None:
+        if patient.get_medication() != "":
             prompt += 'Medication: {}.\n'.format(patient.get_medication())
 
         response = self.gpt.send_prompt(prompt)
 
         return response
-
-    def average_blood_glucose_prompt(self, patient: object) -> str:
-        """Sends an average of CGM readings since last meal to ChatGPT and requests meal suggestions."""
-        pass
 
 if __name__ == '__main__':
     # PATIENT CGM DATA FILE
@@ -71,13 +68,28 @@ if __name__ == '__main__':
     # INSTANTIATE Main OBJECT
     main = Main(api_key, org_id)
 
+    print("Welcome to the Diabetic Meal Planning Portal!\n")
+    print("Fields with \"*\" are required.\nAll other fields are optional and may be left blank.\n")
+    print("#", "-"*35, "#")
+    profile_number = input("Enter Profile Number* (1-4): ")
+    patient_number = input("Enter Patient Number* (0-9): ")
+    last_meal_time = input("Enter Time of Last Meal* (formatted hh:mm AM/PM): ")
+    meal_inquiry = input("Enter Meal about to be Eaten* (i.e., breakfast, lunch, or dinner): ")
+    dietary_restrictions = input("Enter Dietary Restrictions (separated by commas): ")
+    budget = input("Enter Meal Budget $: ")
+    exercise = input("Enter any upcoming exercise or notable physical activity: ")
+    medication = input("Enter any medications (with amounts) being taken: ")
+    print("#", "-"*35, "#")
+
     # INSTANTIATE Patient OBJECT
-    last_meal_time = datetime.strptime('3:00 PM', '%I:%M %p')
-    diabetic_patient = Patient(dh.get_data_by_patient(patient=0, profile=4), last_meal_time=last_meal_time, dietary_restrictions='vegan', budget='20')
+    last_meal_time = datetime.strptime(last_meal_time, '%I:%M %p')
+    patient_data = dh.get_data_by_patient(patient=int(patient_number), profile=int(profile_number))
+    diabetic_patient = Patient(patient_data, last_meal_time, budget, exercise, dietary_restrictions, medication)
 
     # GET DINNER MEAL RECOMMENDATIONS FOR diabetic_patient
-    #response = main.instantaneous_blood_glucose_prompt(diabetic_patient, 'dinner')
-    #print(response)
+    response = main.get_meal_suggestions(diabetic_patient, meal_inquiry)
+    print(response)
+    print("#", "-"*35, "#")
 
     # SEE HOW CHATGPT INTERPRETS BLOOD-GLUCOSE LEVELS
     #log = main.blood_glucose_increment_test()
