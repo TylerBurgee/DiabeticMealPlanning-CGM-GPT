8685f8e92b43732e8fa25e5c98bb02eccafe4379
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2023-07-21 13:04:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class Main:
         avg_blood_glucose = 'Average blood-glucose since last meal: {} mg/dL.\n'.format(patient.get_avg_blood_glucose_last_meal())
         min_blood_glucose = 'Minimum blood-glucose since last meal: {} mg/dL.\n'.format(patient.get_min_blood_glucose_last_meal())
         max_blood_glucose = 'Maximum blood-glucose since last meal: {} mg/dL.\n'.format(patient.get_max_blood_glucose_last_meal())
-        last_meal = 'Last meal: {} hr ago.\n'.format(patient.get_hours_since_last_meal())
+        last_meal = 'Last mealtime: {}.\n'.format(patient.get_last_meal_time_str())
         query = 'Query: Is this safe? If so, recommend meals for {} (If blood-glucose is severely low or high, prioritize quickly raising or lowering blood-sugar levels, respectively, before eating a meal); if not, what should I do (prioritize seeking medical attention over remedies)?\n'.format(meal_inquiry)
 
         prompt = current_blood_glucose + start_blood_glucose + avg_blood_glucose + min_blood_glucose + max_blood_glucose + last_meal + query
